71a098dd8d484f7296299ae3bde5e8f57f0814a5
John Houser <johnhouser@wireless-10-145-120-231.public.utexas.edu>
2017-02-06 15:57:08 -0600

    better data and error handling

--- fetch_links_to_recipes.py
+++ fetch_links_to_recipes.py
@@ -6,55 +6,61 @@ import numpy as np
 
 base_html='http://allrecipes.com'
 
+def fetch_links(category,header,df_rec_links,q):
 
-def fetch_links(cat,header,df_rec_links,q):
-	for m in cat:
-		cat= np.unique(df_cat[df_cat[header+'links']==m][header+'category'])
-		for page in range(50):
-			print m + ' page='+str(page)
-			html = base_html+m+'/?page='+str(page)
+	for m in category:
+		
+			cat = np.unique(df_cat[df_cat[header+'links']==m][header+'category'].values)
+			for page in np.arange(0,100):
+				print m + ' page='+str(page)
+				html = base_html+m+'/?page='+str(page)
 
-			r= requests.get(html)
+				r = requests.get(html)
 
-			c=r.content
+				c = r.content
 
+				soup = BeautifulSoup(c,'html.parser')
 
-			soup = BeautifulSoup(c,'html.parser')
+				results = soup.find_all('section','error-page')
+				if len(results)>0:
+					break
+				
+				results = soup.find_all('article','grid-col--fixed-tiles')
 
-			results = soup.find_all('section','error_page')
-			if len(results)>0:
-				break
-			
-			results = soup.find_all('article','grid-col--fixed-tiles')
+				for r in results:
+					if type(r.a)!=type(None) and r.img.has_attr('title'):
+			 				if r.a["href"] not in list(df_rec_links['link']):
+								df_rec_links.loc[q,'link']=r.a["href"]
+								df_rec_links.loc[q,'title']=r.img["title"].encode('ascii','backslashreplace')
+								df_rec_links.loc[q,'category']=cat[0]
+								
+							else:
+								index=df_rec_links[df_rec_links['link']==r.a["href"]].index[0]
+								if df_rec_links.loc[index,'category']!=str(cat[0]):
+									df_rec_links.loc[q,'link']=r.a["href"]
+									df_rec_links.loc[q,'title']=r.img["title"].encode('ascii','backslashreplace')
+									df_rec_links.loc[q,'category']=cat[0]
+								
 
-
-
-			for r in results:
-				if type(r.a)!=type(None) and r.img.has_attr('title'):
-		 				if r.a["href"] not in list(df_rec_links['link']):
-							df_rec_links.loc[q,'link']=r.a["href"]
-							df_rec_links.loc[q,'title']=r.img["title"]
-							df_rec_links.loc[q,'category']=master_cat
-							q=q+1
-						
 							
-			time.sleep(np.random.random()*10)
+							q=q+1
+					df_rec_links.to_csv('recipe_links.csv')	
+
+				time.sleep(np.random.random()*5)
 
 
-		df_rec_links.to_csv('recipe_links.csv')
-
-		return df_rec_links
+	return df_rec_links
 
 df_cat=pd.read_csv('./category_list.csv')
-
 df_rec_links = pd.DataFrame(columns=['link','title','category'])
+#df_rec_links = pd.read_csv('recipe_links.csv').set_index('Unnamed: 0')
 
 master_cat = np.unique(df_cat['master links'])
+master_cat = master_cat
+cat = np.unique(df_cat['links'])
 
-cat = np.unique(df_cat['category'])
-
-df_rec_links=fetch_links(master_cat,'master ',df_rec_links,0)
-df_rec_links=fetch_links(cat,'',df_rec_links,len(df_rec_links))
+df_rec_links=fetch_links(master_cat,'master ',df_rec_links,len(df_rec_links)+1)
+df_rec_links=fetch_links(cat,'',df_rec_links,len(df_rec_links)+1)
 
 
 
